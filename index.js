69babbfffb34ff46df4b77800b4c538fabc8857a
Nawod2000 <sanithunawod2323@gmail.com>
2025-06-26 22:31:47 +0530

    create mongoose connection string

--- index.js
+++ index.js
@@ -1,14 +1,27 @@
 import express from 'express';
 import bodyParser from 'body-parser';
+import mongoose from 'mongoose';
 
 const app = express()
 
 app.use(bodyParser.json())
 
+const connectionString = "mongodb+srv://sanithunawod:12345@cluster0.abqpzjk.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
+
+mongoose.connect(connectionString).then(
+    ()=>{
+        console.log("connected to database")
+    }
+
+).catch(
+    ()=>{
+        console.log("failed to connect to database")
+    }
+)
 
 app.listen(5000,(req,res)=>{
 
